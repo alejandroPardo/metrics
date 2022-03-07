3da038e56f5ab12dabfd759ece60eb4574bf21d5
Alejandro Pardo Rodriguez <alejandropro@ext.inditex.com>
2022-03-07 18:34:58 +0100

    Dockerize application and readme update

---
+++ README.md
@@ -0,0 +1,122 @@
+<div id="top"></div>
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/alejandroPardo/metrics">
+    <img src="metrics-front/public/logo.svg" alt="Logo" width="80" height="80">
+  </a>
+
+<h3 align="center">Metrics App</h3>
+
+  <p align="center">
+    Basic React app with Spring-boot backend and PostgreSQL database to receive, store and publish metrics by week, day, hour and minute.
+  </p>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+        <li><a href="#mock-metrics-generator">Mock metrics generator</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+![Metrics Screen Shot][product-screenshot]
+
+### Built With
+
+- [React.js](https://reactjs.org/)
+- [Bootstrap](https://getbootstrap.com)
+- [Spring-Boot](https://spring.io/projects/spring-boot)
+- [PostgreSQL](https://www.postgresql.org/)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+Follow this instructions to execute this project locally. It is dockerized, so very little configuration is needed.
+
+### Prerequisites
+
+To execute the project all that is required is Docker.
+Download your preferred Docker application and make sure docker-compose is available and you should have no problems.
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/alejandroPardo/metrics.git
+   ```
+2. Execute docker compose to launch the services
+   ```sh
+   docker-compose up
+   ```
+3. That's it!
+   ```
+   FrontEnd: http://localhost
+   BackEnd: http://localhost:8080
+   ```
+
+### Mock metrics generator
+
+The docker compose file has a parameter for a mock generator of metrics, it can be activated by setting to true the flag GENERATE_METRICS in the backend part of the docker-compose
+
+```
+GENERATE_METRICS: 'false'
+```
+
+It is deactivated by default.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- USAGE EXAMPLES -->
+
+## Usage
+
+This is a basic example app. Used mainly to test technologies and show data. It should't be used as is in any case.
+
+It's composed of 3 view,
+
+- A Summarized metrics view, showing the average time that all metrics required to execute in an instant of time, the amount of calls made in that same instant and a table showing the requests made in that period. The period can be defined in a select form at the top of the view
+
+- A Failed metrics view, showing the average time that all failed metrics required to execute in an instant of time, the amount of failed requests made in that same instant and a table showing the failed requests made in that period. The period can be defined in a select form at the top of the view
+
+- A Transactions view, showing all the transactions made in a period of time, with separations in each type of metric and a table with all the transactions. When clicking this transactions, a new modal shows the timeline for the metric selected, as well as information for each transaction in that metric.
+![Transactions Screen Shot][transactions-screenshot]
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- CONTACT -->
+
+## Contact
+
+Your Name - [@alejandropardo](https://twitter.com/alejandropardo) - alejandro.pardo.r@gmail.com
+
+Project Link: [https://github.com/alejandroPardo/metrics](https://github.com/alejandroPardo/metrics)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+[product-screenshot]: metrics-front/public/application.png
+[transactions-screenshot]: metrics-front/public/transactions.png
--- metrics-back/src/main/java/dev/alejandropardo/metrics/batch/GenerateMetrics.java
+++ metrics-back/src/main/java/dev/alejandropardo/metrics/batch/GenerateMetrics.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +25,9 @@ public class GenerateMetrics {
 	private MetricsService metricsService;
 	
 	Random rand = new Random();
+	
+	@Value("${GENERATE_METRICS:false}")
+	private boolean generateMetrics;
 	
 	/*requests/exceptions*/
 	private List<String> urls = List.of("v1/api/sales", "/v1/api/units", "/v1/api/returns", "/v1/api/companies", "v1/api/stock", "v1/api/employees", "v1/api/metrics", "v1/api/reports", "v1/api/tasks", "v1/api/schedule");
@@ -48,24 +52,25 @@ public class GenerateMetrics {
 	
 	@Scheduled(fixedDelayString = "#{new Double((T(java.lang.Math).random() + 1) * 1000).intValue()}")
 	public void generateOKRequests() {
-		Integer duration = rand.nextInt(highResponseTime-lowResponseTime) + lowResponseTime;
-		String requestURL = methods.get(rand.nextInt(methods.size())) + " " + urls.get(rand.nextInt(urls.size()));
-		LocalDateTime requestTime = LocalDateTime.now();
-		List<TransactionRequest> transactions = new ArrayList<>();
-		Integer durationAux = duration;
-		LocalDateTime requestTimeAux = requestTime;
-		Integer transactionNumber = rand.nextInt(5);
-		for(int i = 0; i < transactionNumber-1; i++) {
-			Integer durationStep = rand.nextInt(durationAux);
-			durationAux -= durationStep;
-			requestTimeAux = requestTimeAux.plus(durationStep, ChronoField.MILLI_OF_DAY.getBaseUnit());
-			transactions.add(generateTransaction(false, durationStep, requestURL, requestTimeAux));
+		if(generateMetrics) {
+			Integer duration = rand.nextInt(highResponseTime-lowResponseTime) + lowResponseTime;
+			String requestURL = methods.get(rand.nextInt(methods.size())) + " " + urls.get(rand.nextInt(urls.size()));
+			LocalDateTime requestTime = LocalDateTime.now();
+			List<TransactionRequest> transactions = new ArrayList<>();
+			Integer durationAux = duration;
+			LocalDateTime requestTimeAux = requestTime;
+			Integer transactionNumber = rand.nextInt(5);
+			for(int i = 0; i < transactionNumber-1; i++) {
+				Integer durationStep = rand.nextInt(durationAux);
+				durationAux -= durationStep;
+				requestTimeAux = requestTimeAux.plus(durationStep, ChronoField.MILLI_OF_DAY.getBaseUnit());
+				transactions.add(generateTransaction(false, durationStep, requestURL, requestTimeAux));
+			}
+			transactions.add(generateTransaction(true, durationAux, requestURL, requestTimeAux));
+			
+			MetricRequest req = new MetricRequest(UUID.randomUUID().toString(), requestURL, requestTime, "Response 200 OK", duration, transactions);
+			metricsService.insertMetric(req);
 		}
-		transactions.add(generateTransaction(true, durationAux, requestURL, requestTimeAux));
-		
-		MetricRequest req = new MetricRequest(UUID.randomUUID().toString(), requestURL, requestTime, "Response 200 OK", duration, transactions);
-		metricsService.insertMetric(req);
-		
 	}
 	
 	private TransactionRequest generateTransaction(boolean isRequest, Integer duration, String url, LocalDateTime requestTime) {
@@ -92,37 +97,39 @@ public class GenerateMetrics {
 
 	@Scheduled(fixedDelayString = "#{new Double((T(java.lang.Math).random() + 1) * 4000).intValue()}")
 	public void generateKORequests() {
-		String koValue = responseCodesKO.get(rand.nextInt(responseCodesKO.size()));
-		String requestURL = methods.get(rand.nextInt(methods.size())) + " " + urls.get(rand.nextInt(urls.size()));
-		LocalDateTime requestTime = LocalDateTime.now();
-		TransactionRequest request = new TransactionRequest(LogType.REQUEST.toString(), null, requestURL, requestTime, null, Integer.parseInt(koValue));
-		List<TransactionRequest> transactions = new ArrayList<>();
-		
-		Integer duration = lowResponseTime;
-		switch(koValue) {
-		case "502":
-			duration = highResponseTime + 1;
-			transactions.add(new TransactionRequest(LogType.EXCEPTION.toString(), null, dependencies.get(rand.nextInt(dependencies.size())), requestTime, "Timeout retrieving data from: select " + tables.get(rand.nextInt(tables.size())) + " " + tables.get(rand.nextInt(tables.size())) + " from schema."+tables.get(rand.nextInt(tables.size())), null));
-			transactions.add(request);
-			break;
-		case "400":
-			duration = rand.nextInt(highResponseTime-lowResponseTime) + lowResponseTime;
-			transactions.add(new TransactionRequest(LogType.TRACE.toString(), LogLevel.ERROR.toString(), "LOG ERROR", requestTime, koLogs.get(rand.nextInt(koLogs.size())), null));
-			transactions.add(request);
-			break;
-		case "401":
-			transactions.add(new TransactionRequest(LogType.TRACE.toString(), LogLevel.ERROR.toString(), "LOG ERROR", requestTime, unauthorizedLogs.get(rand.nextInt(unauthorizedLogs.size())), null));
-			transactions.add(request);
-			break;
-		case "403":
-			transactions.add(new TransactionRequest(LogType.TRACE.toString(), LogLevel.ERROR.toString(), "LOG ERROR", requestTime, forbiddenLogs.get(rand.nextInt(forbiddenLogs.size())), null));
-			transactions.add(request);
-			break;
-		default:
-			break;
+		if(generateMetrics) {
+			String koValue = responseCodesKO.get(rand.nextInt(responseCodesKO.size()));
+			String requestURL = methods.get(rand.nextInt(methods.size())) + " " + urls.get(rand.nextInt(urls.size()));
+			LocalDateTime requestTime = LocalDateTime.now();
+			TransactionRequest request = new TransactionRequest(LogType.REQUEST.toString(), null, requestURL, requestTime, null, Integer.parseInt(koValue));
+			List<TransactionRequest> transactions = new ArrayList<>();
+			
+			Integer duration = lowResponseTime;
+			switch(koValue) {
+			case "502":
+				duration = highResponseTime + 1;
+				transactions.add(new TransactionRequest(LogType.EXCEPTION.toString(), null, dependencies.get(rand.nextInt(dependencies.size())), requestTime, "Timeout retrieving data from: select " + tables.get(rand.nextInt(tables.size())) + " " + tables.get(rand.nextInt(tables.size())) + " from schema."+tables.get(rand.nextInt(tables.size())), null));
+				transactions.add(request);
+				break;
+			case "400":
+				duration = rand.nextInt(highResponseTime-lowResponseTime) + lowResponseTime;
+				transactions.add(new TransactionRequest(LogType.TRACE.toString(), LogLevel.ERROR.toString(), "LOG ERROR", requestTime, koLogs.get(rand.nextInt(koLogs.size())), null));
+				transactions.add(request);
+				break;
+			case "401":
+				transactions.add(new TransactionRequest(LogType.TRACE.toString(), LogLevel.ERROR.toString(), "LOG ERROR", requestTime, unauthorizedLogs.get(rand.nextInt(unauthorizedLogs.size())), null));
+				transactions.add(request);
+				break;
+			case "403":
+				transactions.add(new TransactionRequest(LogType.TRACE.toString(), LogLevel.ERROR.toString(), "LOG ERROR", requestTime, forbiddenLogs.get(rand.nextInt(forbiddenLogs.size())), null));
+				transactions.add(request);
+				break;
+			default:
+				break;
+			}
+			MetricRequest metric = new MetricRequest(UUID.randomUUID().toString(), requestURL, requestTime, "RESPONSE "+koValue, duration, transactions);
+			metricsService.insertMetric(metric);
 		}
-		MetricRequest metric = new MetricRequest(UUID.randomUUID().toString(), requestURL, requestTime, "RESPONSE "+koValue, duration, transactions);
-		metricsService.insertMetric(metric);
 	}
 	
 }
